403fbf8878b0093cb94466469d52752d19b9409d
Amazing_dev <71583327+AestraDev@users.noreply.github.com>
2021-03-25 13:34:50 +0530

    Updated Index.js
    updated index.js according to our new video [ game activity and status ]

--- index.js
+++ index.js
@@ -65,6 +65,31 @@ client.on("message", message => {
     return message.channel.send("Subscribe to aestra-tech");
   }
 });
+//------status
+client.on("ready", () => {
+  console.log(`Hey user {client.user.username} is online`);
+  client.user.setPresence({
+    activity : {
+      name: "Aestra Tech",
+      
+      // -----ALL TYPES----
+      // PLAYING
+      // WATCHING
+      // STREAMING
+      // LISTENING
+      
+      type: "WATCHING"
+    },
+    
+    //TYPES
+    // -- dnd ( do not disturb )
+    // -- idle
+    // -- invisible
+    // -- online
+    
+    status : 'dnd'
+  })
+})
     
 
 
